e9902382f83f45302a4ca3ee68d768c59e16b285
Simone Cadetto <cadetto.simone@gmail.com>
2019-04-02 18:17:57 +0200

    Eliminato file java Hello, Creato file java somma, aggiunto Hello cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+/**
+* @author Simone Cadetto
+* @date 03/04/2019
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world" << endl;
+    return 0;
+}
---
+++ Somma.java
@@ -0,0 +1,16 @@
+/** 
+ * @author Simone Cadetto
+ * @date 03/04/2019
+ */
+
+public class Somma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 22;
+		int b = 43;
+		
+		int som = a + b;
+		System.out.println("Somma: " + som);
+	}
+}
